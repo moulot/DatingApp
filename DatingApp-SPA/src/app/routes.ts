6d4e45e1f1cfa4ee4b106ecdeb3d4300e1f5a9f1
Georges Moulot <gmoulot@gmail.com>
2019-04-12 23:56:56 +0000

    End of section 7

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -16,7 +16,6 @@ export const appRoutes: Routes = [
             {path: 'members', component: MemberListComponent},
             {path: 'messages', component: MessagesComponent},
             {path: 'lists', component: ListsComponent},
-        
-    }
+    },
     {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
